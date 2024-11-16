7a6f302aef40ff53f580ab94c79f21ec4e38670b
axel <axssel.diazfl@gmail.com>
2024-11-16 14:31:31 -0800

    cleck, sanity and shadcn set up

---
+++ app/(store)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      e-commerce app :D
+      <Button>Click me</Button>
+    </div>
+  );
+}
